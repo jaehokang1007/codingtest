9de59325577bb2ae38840b128ef7802184d706a0
jaehokang1007 <165138539+jaehokang1007@users.noreply.github.com>
2024-06-14 12:14:36 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner; // 스캐너
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // 스캐너
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int A = sc.nextInt(); // 입력받을 A 선언
+        int B = sc.nextInt(); // 입력받을 B 선언
         
-        System.out.print(A + B);
+        System.out.print(A + B); // A + B 출력
     }
 }
