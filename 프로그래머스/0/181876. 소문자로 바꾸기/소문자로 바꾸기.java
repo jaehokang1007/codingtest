525368389b1e7f822ea5f9d05b9f162ac335c40c
jaehokang1007 <165138539+jaehokang1007@users.noreply.github.com>
2024-07-08 19:01:15 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String solution(String myString) {
+        
+        String answer = "";
+        
+        answer = myString.toLowerCase(); // 소문자 변환 함수
+        
+        return answer;
+    }
+}
