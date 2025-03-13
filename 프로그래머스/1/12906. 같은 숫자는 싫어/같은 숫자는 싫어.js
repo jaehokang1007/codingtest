2c84353849b4ed3cd6f256f9f61c3ff6312bea93
jaehokang1007 <165138539+jaehokang1007@users.noreply.github.com>
2025-03-14 01:34:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.76 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    var answer = [];
+    answer.push(arr[0]);
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i-1] != arr[i]) answer.push(arr[i]);
+    }
+    return answer;
+}
